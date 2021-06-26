b097b0740110b91e6b4d788fbe810b69a48c8996
Mladenec1977 <rozgon1977@gmail.com>
2021-06-26 17:30:59 +0300

    Level2vue1

---
+++ src/components/List.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="row">
+    <div class="pillar"
+         v-for="list in lists"
+         :key="list.id">
+      <h2>{{ list.title }}</h2>
+      <Card class="card"
+            v-for="card in list.cards"
+            :key="card.id"
+            :title="card.title"/>
+      <button>+ добавить карточку</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+import { ICard } from '@/common/interfaces/card.d.ts';
+import Card from '@/components/Card.vue';
+
+export default Vue.extend({
+  name: 'List',
+  props: {
+    id: Number,
+    title: String,
+    cards: Object as PropType<ICard[]>,
+  },
+  data: () => ({
+    lists: [
+      {
+        id: 1,
+        title: 'Планы',
+        cards: [
+          { id: 1, title: 'помыть кота' },
+          { id: 2, title: 'приготовить суп' },
+          { id: 3, title: 'сходить в магазин' },
+        ],
+      },
+      {
+        id: 2,
+        title: 'В процессе',
+        cards: [
+          { id: 4, title: 'посмотреть сериал' },
+        ],
+      },
+      {
+        id: 3,
+        title: 'Сделано',
+        cards: [
+          { id: 5, title: 'сделать домашку' },
+          { id: 6, title: 'погулять с собакой' },
+        ],
+      },
+    ],
+  }),
+  components: {
+    Card,
+  },
+});
+
+</script>
+
+<style>
+.card {
+  background: antiquewhite;
+  width: 80%;
+  padding: 2px;
+  margin: 5px auto;
+}
+.pillar {
+  background: aqua;
+  width: 200px;
+  height: 50ex;
+  margin: 20px;
+}
+.row {
+  display: flex;
+  flex-direction: row;
+}
+</style>
---
+++ src/views/Board.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="board">
+    <h2>{{ title }}</h2>
+    <div class="row">
+      <List />
+      <div>
+        <button>+ добавить список</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import List from '@/components/List.vue';
+
+export default Vue.extend({
+  name: 'Board',
+  data() {
+    return {
+      title: 'Моя тестовая доска',
+    };
+  },
+  components: {
+    List,
+  },
+});
+</script>
+
+<style>
+.board {
+  height: 93%;
+  margin: 5px 20px;
+}
+.row {
+  display: flex;
+  flex-direction: row;
+}
+
+button {
+  background: chartreuse;
+  padding: 4px;
+}
+</style>
